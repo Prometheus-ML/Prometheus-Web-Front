186a5f866d7b02f9ebde8a8ea53ec2cbc7e0fd8d
Nicole Lee <nicolelee2001@gmail.com>
2023-01-15 16:03:53 +0900

    Edit member info

--- pages/member.vue
+++ pages/member.vue
@@ -39,6 +39,149 @@
     </ul> -->
 
     <div class="flex justify-between p-6 text-3xl">
+      <p>2기</p>
+      <!-- <font-awesome-icon icon="fa-solid fa-angle-down" /> -->
+    </div>
+    <hr />
+    <div>
+      <div class="p-16">
+        <ul class="flex flex-wrap justify-center gap-9">
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_002.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 홍보부장</p>
+            <p class="text-xl mb-2">변가은</p>
+            <p class="text-l mb-2">
+              성균관대학교 건축학과, 데이터사이언스 융합전공
+            </p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_003.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 차장</p>
+            <p class="text-xl mb-2">심수민</p>
+            <p class="text-l mb-2">연세대학교 행정학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_001.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 기획부 부원</p>
+            <p class="text-xl mb-2">황보률</p>
+            <p class="text-l mb-2">연세대학교 인공지능학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_004.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 부원</p>
+            <p class="text-xl mb-2">김태윤</p>
+            <p class="text-l mb-2">서울대학교 컴퓨터공학부</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_005.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2"></p>
+            <p class="text-xl mb-2">김종효</p>
+            <p class="text-l mb-2">성균관대학교 소프트웨어학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_006.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2"></p>
+            <p class="text-xl mb-2">여신호</p>
+            <p class="text-l mb-2">세종대학교 정보보호학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_007.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2"></p>
+            <p class="text-xl mb-2">이성민</p>
+            <p class="text-l mb-2">한국외국어대학교 컴퓨터공학부</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_008.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 부원</p>
+            <p class="text-xl mb-2">최지웅</p>
+            <p class="text-l mb-2">한국외국어대학교 포르투갈어과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_009.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 섭외부 부원</p>
+            <p class="text-xl mb-2">오유진</p>
+            <p class="text-l mb-2">이화여자대학교 인공지능학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_010.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 부원</p>
+            <p class="text-xl mb-2">배윤호</p>
+            <p class="text-l mb-2">한양대학교 정책학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_011.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 부원</p>
+            <p class="text-xl mb-2">연유정</p>
+            <p class="text-l mb-2">고려대학교 산업경영공학부</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_012.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 개발부 부원</p>
+            <p class="text-xl mb-2">김준희</p>
+            <p class="text-l mb-2">숙명여자대학교 컴퓨터과학전공</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_013.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2"></p>
+            <p class="text-xl mb-2">권미주</p>
+            <p class="text-l mb-2">동덕여자대학교 정보통계학과</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/2_014.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">2기 섭외부 부원</p>
+            <p class="text-xl mb-2">최태은</p>
+            <p class="text-l mb-2">
+              연세대학교 언더우드국제대학, 경제학 전공, 컴퓨터과학 복수전공
+            </p>
+          </li>
+        </ul>
+      </div>
+      <hr />
+    </div>
+
+    <div class="flex justify-between p-6 text-3xl">
       <p>1기</p>
       <!-- <font-awesome-icon icon="fa-solid fa-angle-down" /> -->
     </div>
@@ -80,7 +223,7 @@
             />
             <p class="text-sm mb-2">1기 개발부 차장</p>
             <p class="text-xl mb-2">조현우</p>
-            <p class="text-l mb-2">세종대학교 조현우</p>
+            <p class="text-l mb-2">세종대학교 조인공지능학과</p>
           </li>
           <li class="w-64 text-center">
             <img
@@ -123,7 +266,7 @@
               src="@/assets/images/member/1_009.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">1, 2기 개발부 부원</p>
             <p class="text-xl mb-2">조동현</p>
             <p class="text-l mb-2">세종대학교 지능기전공학부</p>
           </li>
@@ -161,7 +304,7 @@
               src="@/assets/images/member/1_013.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">2기 개발부 부원</p>
             <p class="text-xl mb-2">황영훈</p>
             <p class="text-l mb-2">한양대학교 철학과 / 스타트업 백엔드 인턴</p>
           </li>
@@ -179,7 +322,7 @@
               src="@/assets/images/member/1_015.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">1, 2기 기획부 부원</p>
             <p class="text-xl mb-2">김수빈</p>
             <p class="text-l mb-2">연세대학교 문헌정보학과, 산업공학과</p>
           </li>
@@ -188,7 +331,7 @@
               src="@/assets/images/member/1_016.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">1, 2기 기획부 부원</p>
             <p class="text-xl mb-2">김은비</p>
             <p class="text-l mb-2">한국외국어대학교 컴퓨터전자시스템공학부</p>
           </li>
@@ -206,7 +349,7 @@
               src="@/assets/images/member/1_018.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">2기 섭외부 부원</p>
             <p class="text-xl mb-2">박지완</p>
             <p class="text-l mb-2">한국외국어대학교 영어학과</p>
           </li>
@@ -235,7 +378,7 @@
               src="@/assets/images/member/1_021.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">2기 섭외부 부원</p>
             <p class="text-xl mb-2">양지연</p>
             <p class="text-l mb-2">
               한양대학교 생체공학과 / 서울대학교 뇌인지과학과 석사 과정
@@ -255,7 +398,7 @@
               src="@/assets/images/member/1_023.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
-            <p class="text-sm mb-2"></p>
+            <p class="text-sm mb-2">2기 섭외부 부원</p>
             <p class="text-xl mb-2">정민수</p>
             <p class="text-l mb-2">고려대학교 생명공학과</p>
           </li>
@@ -266,150 +409,34 @@
             />
             <p class="text-sm mb-2"></p>
             <p class="text-xl mb-2">김준형</p>
-            <p class="text-l mb-2">상명대학교 컴포터과학과</p>
+            <p class="text-l mb-2">상명대학교 컴퓨터과학과</p>
           </li>
-        </ul>
-      </div>
-      <hr />
-    </div>
-
-    <div class="flex justify-between p-6 text-3xl">
-      <p>2기</p>
-      <!-- <font-awesome-icon icon="fa-solid fa-angle-down" /> -->
-    </div>
-    <hr />
-    <div>
-      <div class="p-16">
-        <ul class="flex flex-wrap justify-center gap-9">
           <li class="w-64 text-center">
             <img
-              src="@/assets/images/member/2_001.jpeg"
+              src="@/assets/images/member/1_025.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">1, 2기 개발부 부원</p>
+            <p class="text-xl mb-2">김선웅</p>
+            <p class="text-l mb-2">한양대학교 인공지능학과 석사과정</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/1_026.jpeg"
+              class="w-full bg-gray-300 mb-5 rounded-md"
+            />
+            <p class="text-sm mb-2">1, 2기 개발부 부원</p>
+            <p class="text-xl mb-2">한재연</p>
+            <p class="text-l mb-2">한양대학교 수학과 / NAVER 취업</p>
+          </li>
+          <li class="w-64 text-center">
+            <img
+              src="@/assets/images/member/1_027.jpeg"
               class="w-full bg-gray-300 mb-5 rounded-md"
             />
             <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">황보률</p>
-            <p class="text-l mb-2">연세대학교 인공지능학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_002.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2">2기 홍보부장</p>
-            <p class="text-xl mb-2">변가은</p>
-            <p class="text-l mb-2">
-              성균관대학교 건축학과, 데이터사이언스 융합전공
-            </p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_003.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2">2기 개발부 차장</p>
-            <p class="text-xl mb-2">심수민</p>
-            <p class="text-l mb-2">연세대학교 행정학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_004.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">김태윤</p>
-            <p class="text-l mb-2">서울대학교 컴퓨터공학부</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_005.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">김종효</p>
-            <p class="text-l mb-2">성균관대학교 소프트웨어학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_006.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">여신호</p>
-            <p class="text-l mb-2">세종대학교 정보보호학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_007.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">이성민</p>
-            <p class="text-l mb-2">한국외국어대학교 컴퓨터공학부</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_008.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">최지웅</p>
-            <p class="text-l mb-2">한국외국어대학교 포르투갈어과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_009.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">오유진</p>
-            <p class="text-l mb-2">이화여자대학교 인공지능학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_010.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">배윤호</p>
-            <p class="text-l mb-2">한양대학교 정책학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_011.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">연유정</p>
-            <p class="text-l mb-2">고려대학교 산업경영공학부</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_012.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">김준희</p>
-            <p class="text-l mb-2">숙명여자대학교 컴퓨터과학전공</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_013.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">권미주</p>
-            <p class="text-l mb-2">동덕여자대학교 정보통계학과</p>
-          </li>
-          <li class="w-64 text-center">
-            <img
-              src="@/assets/images/member/2_014.jpeg"
-              class="w-full bg-gray-300 mb-5 rounded-md"
-            />
-            <p class="text-sm mb-2"></p>
-            <p class="text-xl mb-2">최태은</p>
-            <p class="text-l mb-2">
-              연세대학교 언더우드국제대학, 경제학 전공, 컴퓨터과학 복수전공
-            </p>
+            <p class="text-xl mb-2">정지영</p>
+            <p class="text-l mb-2">고려대학교 신소재공학부</p>
           </li>
         </ul>
       </div>
