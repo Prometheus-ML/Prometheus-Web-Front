4f712331d58f55987f900268c1ff0b3a877aca2c
Nicole Lee <nicolelee2001@gmail.com>
2023-01-19 00:28:48 +0900

    Add button to open google form

--- pages/hackathon.vue
+++ pages/hackathon.vue
@@ -14,8 +14,15 @@
         <div class="container">
           <p class="text-l mb-2">title</p>
           <p class="text-sm mb-2">
-            texttexttexttexttexttexttexttexttexttexttext
+            text text text text text text text text text text text
           </p>
+          <button
+            type="button"
+            class="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            @click="openGoogleForm()"
+          >
+            참가신청하기
+          </button>
         </div>
       </div>
     </div>
@@ -29,6 +36,10 @@ export default {
   name: "HackathonView",
   components: {},
   data: () => ({}),
-  methods: {},
+  methods: {
+    openGoogleForm: function () {
+      window.open("https://forms.gle/uVSuXEzgZq7VWL3E6");
+    },
+  },
 };
 </script>
