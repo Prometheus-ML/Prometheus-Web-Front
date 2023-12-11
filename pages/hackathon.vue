af0083275fbfd4919ab5bda2e4b9e3c8cb3c8d65
kongms002 <ms020221@naver.com>
2023-12-11 21:41:51 +0900

    hackathon

--- pages/hackathon.vue
+++ pages/hackathon.vue
@@ -3,8 +3,8 @@
 		<img src="@/assets/images/hackathon.png" alt="" class="object-cover w-full">
     <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <p id="test" class="font-bold text-center text-4xl md:text-7xl mb-2 text-white transition-opacity duration-500 ease-in">PROMETHEUS</p>
-      <p class="md:text-4xl text-2xl text-center text-white transition-opacity duration-500 ease-in mb-2">HACKATHON</p>
-      <p class="text-sm text-center text-red-700 transition-opacity duration-500 ease-in">12월 11일 공개 예정</p>
+      <p class="md:text-4xl text-2xl text-center text-white transition-opacity duration-500 ease-in mb-2">2024 HACKATHON</p>
+      <p class="text-sm text-center text-red-700 transition-opacity duration-500 ease-in">12월 18일 공개 예정</p>
     </div>
 
 	</div>
@@ -48,4 +48,3 @@ onMounted(async() => {
 <style scoped>
 /* 필요한 스타일링을 추가할 수 있습니다. */
 </style>
-
