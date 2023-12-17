57048cb29abc7790bce6c1dce79497fd6d31d20b
kongms002 <ms020221@naver.com>
2023-12-17 22:20:30 +0900

    hackathon

--- pages/hackathon/post.vue
+++ pages/hackathon/post.vue
@@ -153,7 +153,7 @@ const postList = ref([
 
 const getPosts = async () => {
   try {
-    const response = await $api(`${import.meta.env.VITE_API_URL}/hackathon/`, {
+    const response = await $api(`${import.meta.env.VITE_API_URL}/hackathon/get_posts`, {
       method: 'GET',
     });
     postList.value = response;
