3fc771977cb1616615ee341b745825db9cb75136
kongms002 <ms020221@naver.com>
2024-01-02 18:05:08 +0900

    hackathon

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -144,39 +144,6 @@
 																				<li v-for="(value, idx) in member.history" :key="idx">{{ value }}</li>
 																		</ol>
 																</div>
-																<!-- Trash Icon -->
-																<svg
-																		@click="deleteMember(member)"
-																		xmlns="http://www.w3.org/2000/svg"
-																		class="h-6 w-6 text-red-600 absolute top-1 right-1 cursor-pointer"
-																		fill="none"
-																		viewBox="0 0 24 24"
-																		stroke="currentColor"
-																>
-																		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-																</svg>
-																<!-- Edit Icon and functionality -->
-																<svg
-																		@click="showEditForm(member.id)"
-																		xmlns="http://www.w3.org/2000/svg"
-																		class="h-6 w-6 text-green-400 absolute bottom-1 right-1 cursor-pointer"
-																		fill="none"
-																		viewBox="0 0 24 24"
-																		stroke="currentColor"
-																>
-																		<path
-																				stroke-linecap="round"
-																				stroke-linejoin="round"
-																				stroke-width="2"
-																				d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-																		/>
-																		<path
-																				stroke-linecap="round"
-																				stroke-linejoin="round"
-																				stroke-width="2"
-																				d="M2 18c2.666-1.333 7.333-1.333 10 0m0 0c2.667 1.333 7.333 1.333 10 0m-10 5v-3a4 4 0 118 0v3"
-																		/>
-																</svg>
 														</div>
 												</div>
 												<div class="w-full h-full pb-2 flex items-center justify-center">
