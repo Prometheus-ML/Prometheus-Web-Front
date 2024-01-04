1d7ca3ddc0dea548206d531693d15e58dc7120f9
kongms002 <ms020221@naver.com>
2024-01-04 12:36:03 +0900

    hackathon

--- pages/hackathon/home.vue
+++ pages/hackathon/home.vue
@@ -13,8 +13,8 @@
           <hr class="p-1 border-t border-neutral-500">
           <div class="grid grid-cols-4 gap-1">
             <div class="py-3 row-span-3 font-semibold text-base md:text-2xl">참가 대상</div>
-            <div class="pt-3 col-span-3 text-sm md:text-xl">- 인원: 약 60~80명 (한 팀당 4~5인으로 구성)</div>
-            <div class="pt-3 col-span-3 text-sm md:text-xl">- 서비스 개발과 인공지능에 관심을 지닌 전국 대학생</div>
+            <div class="pt-3 col-span-3 text-sm md:text-xl">- 본선 참가 인원: 16~20팀 (한 팀당 4~5인으로 구성)</div>
+            <div class="pt-3 col-span-3 text-sm md:text-xl">- 서비스 개발과 인공지능에 관심을 지닌 전국 대학생 및 대학원생 (졸업생 불가)</div>
             <div class="py-3 col-span-3 text-sm md:text-xl">- 본 행사 기간동안 열리는 온/오프라인 행사에 참여가 가능한 자</div>
           </div>
           <hr class="p-1 border-t">
@@ -70,7 +70,7 @@
           <div class="grid grid-cols-4 gap-1">
             <div class="py-3 font-semibold text-base md:text-2xl">본선</div>
             <div class="ml-2 py-4 text-sm md:text-xl">02.02(금) - 02.03(토)</div>
-            <div class="ml-2 py-4 col-span-2 font-light text-xs md:text-lg">오프라인(팁스타운에서 진행)</div>
+            <div class="ml-2 py-4 col-span-2 font-light text-xs md:text-lg">오프라인 (강남 'TIPSTOWN'에서 진행)</div>
           </div>
           <hr class="p-1 border-t">
         </div>
