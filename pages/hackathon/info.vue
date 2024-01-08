2c1adadfbcb58990476f2596dded1d5dfc8c9b21
kongms002 <ms020221@naver.com>
2024-01-08 21:17:48 +0900

    info

--- pages/hackathon/info.vue
+++ pages/hackathon/info.vue
@@ -10,21 +10,21 @@
       <div class="grid grid-cols-4 gap-x-4 container mx-auto">
         <p class="text-xl md:text-3xl font-bold mr-3 md:mr-12">이전 회차 수상작</p>
         <div class="col-span-3 flex-grow">
-          <div class="grid grid-cols-2 gap-x-6">
+          <div class="grid grid-col md:grid-cols-2 gap-x-6">
             <div class="mr-4 my-4 overflow-hidden rounded-lg text-sm md:text-xl flex flex-col justify-center items-center">
-              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-cyan-300 p-3">(1등) 커먼컴퓨터 상 - AI Interprenuers 팀</div>              
+              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-yellow-400 p-3">(1등) 커먼컴퓨터 상 - AI Interprenuers 팀</div>              
               <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold bg-white text-black h-full w-full p-3">AI Enterprenuers	투로잉 : IP 기반 웹툰 텍스트 드로잉 SaaS</div>
             </div>
             <div class="mr-4 my-4 overflow-hidden rounded-lg text-sm md:text-xl flex flex-col justify-center items-center">
-              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-yellow-400 p-3">(2등) 뉴럴웍스랩 상 - 자비스 팀</div>             
+              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-gray-400 p-3">(2등) 뉴럴웍스랩 상 - 자비스 팀</div>             
               <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold bg-white text-black h-full w-full p-3">AI 보컬 트레이닝 솔루션 Progressing</div>
             </div>
             <div class="mr-4 my-4 overflow-hidden rounded-lg text-sm md:text-xl flex flex-col justify-center items-center">
-              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-gray-300 p-3">(3등) AIFactory 상 - 코스메테틱 팀</div>
+              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-yellow-800 p-3">(3등) AIFactory 상 - 코스메테틱 팀</div>
               <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold bg-white text-black h-full w-full p-3">메이크업샵 가상피팅&매칭 서비스 CosMethetic</div>
             </div>
             <div class="mr-4 my-4 overflow-hidden rounded-lg text-sm md:text-xl flex flex-col justify-center items-center">
-              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-gray-300 p-3">(3등) AIFactory 상 - 플러스알파 팀</div>
+              <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold overflow-hidden h-full w-full bg-yellow-800 p-3">(3등) AIFactory 상 - 플러스알파 팀</div>
               <div class="lg:py-4 flex flex-col justify-center text-center text-xs md:text-sm font-semibold bg-white text-black h-full w-full p-3">LIV:Ebook 리비북 : eBook을 읽으면서 생생한 음향 효과를 경험하기까지</div>
             </div>
           </div>
